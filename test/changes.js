0ba43a2387619d3c37e1203577b5bbbf6ff96767
Stefan Kruger <stefan.kruger@uk.ibm.com>
2016-02-04 14:28:22 +0000

    Rewrote test for changes
    Running the changes test revealed one more bug in bulk-docs; fixed.

--- test/changes.js
+++ test/changes.js
@@ -1,84 +1,37 @@
-var util = require('util');
-var assert = require('assert');
-var request = require('supertest');
-var urllib = require('url');
-var async = require('async');
-var _ = require('underscore');
+var assert = require('assert'),
+  auth = require('../lib/auth'),
+  PouchDB = require('pouchdb');
 
-describe('test _changes', function() {
+describe('changes', function () {
+  it('sequence', function () {
+    this.timeout(10000);
+    var docCount = 2;
+    var docs = testUtils.makeDocs(docCount),
+      remoteURL = testUtils.url('bob', auth.sha1('bob')),
+      remote = new PouchDB(remoteURL),
+      seq1 = '',
+      id ,rev;
 
-  it('test basics', function(done) {
-    this.timeout(150000);
-
-    var url = testUtils.url(username, password);
-
-    async.waterfall([
-      // Step 1: insert doc1: owned by foo
-      function (callback) {
-        request(url).put('/' + testUtils.makeDocName())
-          .send({'hello': 'world'})
-          .end(function(err, res) {
-            callback(err, [res]); // args[0]
-          });
-      },
-
-      function (args, callback) {
-        // Step2: invoke _changes: doc1@rev1 seen. save sequence to seq1
-        request(url).get('/_changes')
-          .end(function(err, res) {
-            args.push(res);
-            callback(err, args); // args[1]
-          });
-      },
-
-      function (args, callback) {
-        // Step 3: update doc1
-        var path = '/' + args[0].body.id;
-        request(url).post(path)
-          .send({'new': 'body'})
-          .end(function(err, res) {
-            args.push(res);
-            callback(err, args); // args[2] <-- new rev
-          });
-      },
-
-      function(args, callback) {
-        // Step 4: invoke _changes: doc1@rev1, doc1@rev2 (args[2]) seen
-        request(url).get('/_changes')
-          .end(function(err, res) {
-            args.push(res);
-            callback(err, args); // args[3]
-          });
-      },
-
-      function(args, callback) {
-        var id = args[0].body.id;
-        var row = _.findWhere(args[1].body.results, { 'id': id });
-        if (row) {
-          request(url).get('/_changes?since=' + row.seq)
-            .end(function(err, res) {
-              args.push(res);
-              callback(err, args); // args[4]
-            }
-          );
-        } else {
-          callback(new Error('Failed to locate id ' + id +
-            ' in changes: ' + JSON.stringify(args[1].body)), args);
-        }
-      }
-    ],
-
-    function (err, args) {
-      if (err) {
-        done(err);
-      }
-
-      // assert.ok(args[4].body.results.length === 1);
-      var row = _.findWhere(args[4].body.results, {id: args[0].body.id});
-      assert.ok(row);
-      assert.ok(row.changes[0].rev === args[2].body.rev);
-
-      done();
+    return remote.bulkDocs(docs).then(function (response) {
+      return remote.changes();
+    }).then(function (response) {
+      seq1 = response.last_seq;
+      // Update a document
+      var newDoc = testUtils.makeDocs(1)[0];
+      newDoc._id = response.results[0].id;
+      newDoc._rev = response.results[0].changes[0].rev;
+      return remote.put(newDoc);
+    }).then(function (response) {
+      id = response.id;
+      rev = response.rev;
+      return remote.changes({since: seq1});
+    }).then(function (response) {
+      assert.equal(response.results.length, 1,
+        'Changes feed should contain single entry');
+      assert.equal(response.results[0].id, id,
+        'ID of document should be the one that was updated');
+      assert.equal(response.results[0].changes[0].rev, rev,
+        'Rev of document should be the one that was updated');
     });
   });
 });
